aa5ab52571a2879d6639899b2643de4e97d79aa9
Pavel Mezencev <mezencev24@mail.ru>
2022-03-14 10:38:39 +0300

    add filter feature

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from '../Filter/ExpenseFilter';
 import Card from '../UI/Card';
 import './Expenses.css';
 
 export default function Expenses({ expenses }) {
-  const expensesEls = expenses.map((expense) => (
-    <ExpenseItem expense={expense} key={expense.id} />
-  ));
+  const [currentYear, setCurrentYear] = useState(2022);
 
-  return <Card className="expenses">{expensesEls}</Card>;
+  const expensesEls = expenses
+    .filter((expense) => expense.date.getFullYear() === currentYear)
+    .map((expense) => <ExpenseItem expense={expense} key={expense.id} />);
+
+  return (
+    <Card className='expenses'>
+      <ExpensesFilter
+        selected={currentYear}
+        onSetYear={(year) => setCurrentYear(year)}
+      />
+      {expensesEls}
+    </Card>
+  );
 }
